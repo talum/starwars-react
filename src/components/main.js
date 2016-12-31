629807ad5d2cfc88d7b29731c6322e45ce8110c2
talum <tracy.a.lum@gmail.com>
2016-12-31 17:37:03 -0500

    add star wars characters

--- src/components/main.js
+++ src/components/main.js
@@ -1,19 +1,28 @@
 import React from 'react'
+import axios from 'axios'
+import Movie from './Movie'
 
 class Main extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {results: []}
+  }
+
+  componentWillMount() {
+    axios.get('http://swapi.co/api/people/').then((response) => {
+      var results = response.data.results
+      this.setState({results: results})
+    })
+  }
+
   render() {
+    const movies = this.state.results
+    const movieItems = movies.map((movie, id) => <Movie key={id} movie={movie}/>)
+
     return (
-      <div>
-        Hello
-      </div>
+      <div>{movieItems}</div>
     )
   }
 }
 
-const PureMain = () => {
-  return (
-    <div>Hello pure</div>
-  )
-}
-
-export default PureMain
+export default Main
